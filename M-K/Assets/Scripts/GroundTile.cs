3c0f1e3b4b864e917b1b02147b5c097c24475ab6
katarina-popovic <48888993+katarina-popovic@users.noreply.github.com>
2022-05-20 10:57:26 +0200

    Added collectable item coin

--- M-K/Assets/Scripts/GroundTile.cs
+++ M-K/Assets/Scripts/GroundTile.cs
@@ -12,6 +12,7 @@ public class GroundTile : MonoBehaviour
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();    
         SpawnObstacle();
+        SpawnCoins();
     }
 
     private void OnTriggerExit (Collider other)
@@ -32,5 +33,32 @@ public class GroundTile : MonoBehaviour
         Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
 
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+
+    public GameObject coinPrefab;
+
+    void SpawnCoins()
+    {
+        int coinsToSpawn = 2;
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+        if(point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 1;
+        return point;
     }
 }
